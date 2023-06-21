f24329fcdba57810c8f52234bccd46edb787380c
michelmotta <michelmotta@icloud.com>
2023-06-21 00:12:21 -0300

    initial commit

---
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-card',
+  templateUrl: './pokemon-card.component.html',
+  styleUrls: ['./pokemon-card.component.sass']
+})
+export class PokemonCardComponent {
+  @Input()
+  pokemon!: string;
+  @Input()
+  id!: number;
+  idFormatted!: string;
+
+  getPokemonImage(): string {
+     this.idFormatted = this.leadingZero(this.id);
+    return `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${this.idFormatted}.png`;
+  }
+
+  private leadingZero(str: string | number, size = 3): string {
+    let s = String(str);
+    while (s.length < (size || 2)) {
+      s = '0' + s;
+    }
+    return s;
+  }
+}
